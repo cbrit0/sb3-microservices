4c407a62897a7ea0864333cb025f84acad4d931d
cbrit0 <cristobal.brito.p@gmail.com>
2025-04-20 23:27:42 -0400

    add getAll product endpoint

--- product-service/src/main/java/com/cbrit0/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/cbrit0/microservices/product/controller/ProductController.java
@@ -1,11 +1,13 @@
 package com.cbrit0.microservices.product.controller;
 
 import com.cbrit0.microservices.product.dto.ProductRequest;
-import com.cbrit0.microservices.product.model.Product;
+import com.cbrit0.microservices.product.dto.ProductResponse;
 import com.cbrit0.microservices.product.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/product")
@@ -16,7 +18,13 @@ public class ProductController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Product createProduct(@RequestBody ProductRequest productRequest) {
+    public ProductResponse createProduct(@RequestBody ProductRequest productRequest) {
         return productService.createProduct(productRequest);
+    }
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ProductResponse> getAllProducts() {
+        return productService.getAllProducts();
     }
 }
--- product-service/src/main/java/com/cbrit0/microservices/product/service/ProductService.java
+++ product-service/src/main/java/com/cbrit0/microservices/product/service/ProductService.java
@@ -1,11 +1,14 @@
 package com.cbrit0.microservices.product.service;
 
 import com.cbrit0.microservices.product.dto.ProductRequest;
+import com.cbrit0.microservices.product.dto.ProductResponse;
 import com.cbrit0.microservices.product.model.Product;
 import com.cbrit0.microservices.product.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -13,7 +16,7 @@ import org.springframework.stereotype.Service;
 public class ProductService {
     private final ProductRepository productRepository;
 
-    public Product createProduct(ProductRequest productRequest) {
+    public ProductResponse createProduct(ProductRequest productRequest) {
         Product product = Product.builder()
                 .name(productRequest.name())
                 .description(productRequest.description())
@@ -21,6 +24,13 @@ public class ProductService {
                 .build();
         productRepository.save(product);
         log.info("Product {} is saved", product.getName());
-        return product;
+        return new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice());
+    }
+
+    public List<ProductResponse> getAllProducts() {
+        return productRepository.findAll()
+                .stream()
+                .map(product -> new ProductResponse(product.getId(), product.getName(), product.getDescription(), product.getPrice()))
+                .toList();
     }
 }
