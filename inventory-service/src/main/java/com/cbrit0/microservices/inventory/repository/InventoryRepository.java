0d4edf1953e58038a7cf3289e9fcf6b0ed3bdf6b
cbrit0 <cristobal.brito.p@gmail.com>
2025-04-22 11:27:44 -0400

    add inventory service endpoint

---
+++ inventory-service/src/main/java/com/cbrit0/microservices/inventory/repository/InventoryRepository.java
@@ -0,0 +1,8 @@
+package com.cbrit0.microservices.inventory.repository;
+
+import com.cbrit0.microservices.inventory.model.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface InventoryRepository extends JpaRepository<Inventory, Long> {
+    boolean existsBySkuCodeAndQuantityIsGreaterThanEqual(String skuCode, Integer quantity);
+}
