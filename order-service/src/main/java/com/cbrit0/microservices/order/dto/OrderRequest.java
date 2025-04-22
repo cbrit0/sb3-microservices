1d8a1b848ec978675d97b9d13829dac32103013a
cbrit0 <cristobal.brito.p@gmail.com>
2025-04-22 11:55:42 -0400

    minor order service fix

--- order-service/src/main/java/com/cbrit0/microservices/order/dto/OrderRequest.java
+++ order-service/src/main/java/com/cbrit0/microservices/order/dto/OrderRequest.java
@@ -2,5 +2,5 @@ package com.cbrit0.microservices.order.dto;
 
 import java.math.BigDecimal;
 
-public record OrderRequest(long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
+public record OrderRequest(Long id, String orderNumber, String skuCode, BigDecimal price, Integer quantity) {
 }
--- order-service/src/main/java/com/cbrit0/microservices/order/model/Order.java
+++ order-service/src/main/java/com/cbrit0/microservices/order/model/Order.java
@@ -18,7 +18,7 @@ public class Order {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
     private String orderNumber;
     private String skuCode;
     private BigDecimal price;
