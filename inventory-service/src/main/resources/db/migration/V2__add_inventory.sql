0026d56dbcfcb1e57d172daf59d761ce5fdd41f7
cbrit0 <cristobal.brito.p@gmail.com>
2025-04-22 10:55:12 -0400

    add db init files

---
+++ inventory-service/src/main/resources/db/migration/V2__add_inventory.sql
@@ -0,0 +1,7 @@
+INSERT INTO t_inventory (quantity, sku_code)
+VALUES (100, 'iphone_15'),
+       (100, 'pixel_8'),
+       (100, 'galaxy_s23'),
+       (100, 'oneplus_11'),
+       (100, 'nokia_g50'),
+       (100, 'motorola_edge_30');
